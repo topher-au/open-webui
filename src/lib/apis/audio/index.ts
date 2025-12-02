431d50219db13015a8243819b6b22334cc366fd5
Topher Sheridan <topher@topher.au>
2025-12-03 10:38:54 +1100

    Add params to speech synthesis request

--- src/lib/apis/audio/index.ts
+++ src/lib/apis/audio/index.ts
@@ -101,7 +101,8 @@ export const synthesizeOpenAISpeech = async (
 	token: string = '',
 	speaker: string = 'alloy',
 	text: string = '',
-	model?: string
+	model?: string,
+	params?: object
 ) => {
 	let error = null;
 
@@ -114,7 +115,8 @@ export const synthesizeOpenAISpeech = async (
 		body: JSON.stringify({
 			input: text,
 			voice: speaker,
-			...(model && { model })
+			...(model && { model }),
+			...(params ?? {})
 		})
 	})
 		.then(async (res) => {
